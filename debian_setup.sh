d5283d1d71ab967587a382e6fba6b96a4c32c0f5
Rory Swann <rory@shirenet.io>
2024-02-29 20:54:45 +0000

    First commit.

---
+++ debian_setup.sh
@@ -0,0 +1,102 @@
+#!/usr/bin/env bash
+# ==================================================================
+#  Notes: Very basic. No proper logic. Needs work.
+# ==================================================================
+#
+# Update the system
+sudo apt update && sudo apt upgrade -y
+
+# Install basics
+sudo apt install -y vim kwin-bismuth htop kitty curl wget
+
+# Install Albert
+echo 'deb http://download.opensuse.org/repositories/home:/manuelschneid3r/Debian_12/ /' | sudo tee /etc/apt/sources.list.d/home:manuelschneid3r.list
+curl -fsSL https://download.opensuse.org/repositories/home:manuelschneid3r/Debian_12/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/home_manuelschneid3r.gpg > /dev/null
+sudo apt install -y albert
+
+# Install NeoVim
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
+sudo rm -rf /opt/nvim
+sudo tar -C /opt -xzf nvim-linux64.tar.gz
+echo 'export PATH="$PATH:/opt/nvim/"' >> ~/.bashrc
+rm -f nvim-linux64.tar.gz
+
+# Install AstroVim
+git clone --depth 1 https://github.com/AstroNvim/AstroNvim ~/.config/nvim
+
+# Install GruvBox vim theme
+curl -o ~/.config/nvim/colors/gruvbox.vim https://raw.githubusercontent.com/morhetz/gruvbox/master/colors/gruvbox.vim
+
+# Install Starship
+curl -sS https://starship.rs/install.sh | sh
+
+# Install Nerd Font
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/officialrajdeepsingh/nerd-fonts-installer/main/install.sh)"
+
+# Install LazyGit
+lg_version=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${lg_version}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+sudo install lazygit /usr/local/bin
+rm -f lazygit.tar.gz
+
+# Nvidia stuff
+sudo apt install mokutil openssl nvidia-driver
+mkdir ~/.mok-files
+openssl req -new -x509 -newkey rsa:2048 -keyout ~/.mok-files/MOK.priv -outform DER -out ~/.mok-files/MOK.der -nodes -days 36500 -subj "/CN=${USER}/"
+sudo mokutil --import ~/.mok-files/MOK.der
+
+for module in $(ls /lib/modules/$(uname -r)/updates/dkms/); do
+    sudo /usr/src/linux-headers-$(uname -r)/scripts/sign-file sha256 ./MOK.priv ./MOK.der /lib/modules/$(uname -r)/updates/dkms/${module}
+done
+
+# Install Brave browser
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+sudo apt update
+sudo apt install -y brave-browser
+
+# Install Minikube
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+ echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+    $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+    sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo install minikube-linux-amd64 /usr/local/bin/minikube
+rm -f minikube-linux-amd64
+
+# Install kubectl
+sudo apt install -y apt-transport-https ca-certificates curl
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.29/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt update && sudo apt install -y kubectl
+
+# Install K9s
+sudo apt install -y k9s
+
+# Install Helm
+curl https://baltocdn.com/helm/signing.asc | gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg > /dev/null
+sudo apt-get install apt-transport-https --yes
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+sudo apt-get update
+sudo apt-get install -y helm
+
+# Dotfiles
+here=$(dirname "$(realpath $0)")
+cp -R ${here}/dotfiles/* ~/.config
+
+# Useful aliases
+echo '# Useful aliases' >> ~/.bashrc
+echo 'alias ll="ls -l"' >> ~/.bashrc
+echo 'alias gp="git pull"' >> ~/.bashrc
+
+# Starship says it needs to be last
+echo 'eval "$(starship init bash)"' >> ~/.bashrc
+
