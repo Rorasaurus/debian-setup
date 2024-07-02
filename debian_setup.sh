bb45bd74ff6a58bdc535ada37afc85cd98a3108f
Rory Swann <rswann@rswann-debian-lt.shirenet>
2024-07-02 22:06:32 +0100

    Updated nvim install

--- debian_setup.sh
+++ debian_setup.sh
@@ -18,6 +18,7 @@ sudo apt install -y albert
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
 sudo rm -rf /opt/nvim
 sudo tar -C /opt -xzf nvim-linux64.tar.gz
+sudo mv /opt/nvim-linux64/ /opt/nvim
 echo 'export PATH="$PATH:/opt/nvim/"' >> ~/.bashrc
 rm -f nvim-linux64.tar.gz
 
